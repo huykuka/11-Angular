be68ef240a680d764b2b030e90ad2f01eac85d1e
quanghuy <quanghuy.thai@sick.com>
2023-06-11 15:04:24 +0700

    add sevice injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+import { MessageComponent } from './components/message/message.component';
 
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent,],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessageComponent,],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Hero } from 'src/app/hero';
+import { HEROES } from 'src/app/mock-heroes';
+import { HeroService } from 'src/app/services/hero.service';
+import { MessageService } from 'src/app/services/message.service';
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+
+export class HeroesComponent {
+  constructor(private heroService: HeroService, private messageService:MessageService) {}
+  heroes:Hero[] = [];
+  selectedHero?: Hero
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+    console.log(hero)
+  }
+  getHeroes(): void{
+    this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes)
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+}
