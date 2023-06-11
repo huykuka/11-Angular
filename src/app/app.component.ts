b31a123b83cdf83dbd8c3c26ca6ba4c7fdcf3c14
huykuka <thaiquanghuy.c3a@gmail.com>
2023-06-11 10:47:45 +0700

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = '11-Angular';
---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        xxs: "200px",
+        xs: "480px",
+        sm: "640px",
+        md: "900px",
+        lg: "1000px",
+        xl: "1300px",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      backgroundColor: {
+        "success-500": "#63B017",
+        "error-500": "#EA0823",
+        "secondary-500": "#737F85",
+        "neutral-500": "#8E989D",
+        "neutral-0": "#FFFFFF",
+        "neutral-200": "#C5CACC",
+        "secondary-650": "#50585C",
+        "primary-400": "#009DF4",
+        "primary-500": "#007CC1",
+        "neutral-1": "#FBFCFC",
+      },
+      color: {
+        "success-500": "#63B017",
+        "error-500": "#EA0823",
+        "secondary-500": "#737F85",
+        "neutral-500": "#8E989D",
+        "neutral-0": "#FFFFFF",
+        "neutral-200": "#C5CACC",
+        "secondary-650": "#50585C",
+        "primary-400": "#009DF4",
+        "primary-500": "#007CC1",
+        "neutral-1": "#FBFCFC",
+      },
+
+      fontFamily: {
+        sans: ['"Open Sans"', "sans-serif"],
+      },
+    },
+  },
+  fontFamily: {
+    sans: ['"Open Sans"', "sans-serif"],
+  },
+
+  plugins: [],
+};
